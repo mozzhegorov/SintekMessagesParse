191da1753e187fbc10967c318b6076f6d9d5a901
mail@mail.ru <mail@mail.ru>
2022-08-17 16:05:34 +0100

    restart always

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ git pull origin
 docker build -t sintekmessagesparse .
 docker stop sintekmsgs
 docker rm sintekmsgs
-docker run --name sintekmsgs -p 5005:5000 -d sintekmessagesparse
+docker run --name sintekmsgs -p 5005:5000 -d --restart=always sintekmessagesparse
